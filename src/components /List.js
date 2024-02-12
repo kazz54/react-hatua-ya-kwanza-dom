2f933637b42232cd07b30ea751bfa2da883ea570
kazz54 <jonathan.mrutu@gmail.com>
2024-02-12 11:15:44 +0300

    Create List.js

---
+++ src/components /List.js
@@ -0,0 +1,12 @@
+function List() {
+  return (
+    <>
+      <img
+        class="img-fluid"
+        src="https://source.unsplash.com/cZkdauWij50/200x200"
+      />
+    </>
+  );
+}
+
+export default List;
