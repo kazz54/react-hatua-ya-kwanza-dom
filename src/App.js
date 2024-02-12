b5656268970c2e5744b92582c133bc548b8413c6
kazz54 <jonathan.mrutu@gmail.com>
2024-02-12 11:18:19 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import List from "./components/List";
+
+export default function App() {
+  return (
+    <div className="App">
+      <List />
+      <List />
+     <List />
+    </div>
+  );
+}
